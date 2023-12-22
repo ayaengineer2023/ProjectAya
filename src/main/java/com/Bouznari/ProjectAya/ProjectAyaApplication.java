8ac4fa92a768018b89c0f647081a75c9423e0543
Aya <aya.bouzi@yahoo.com>
2023-12-22 17:36:08 +0100

    Ajouter le squelette Spring Boot

---
+++ src/main/java/com/Bouznari/ProjectAya/ProjectAyaApplication.java
@@ -0,0 +1,13 @@
+package com.Bouznari.ProjectAya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bouznari/ProjectAya/ProjectAyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bouznari.ProjectAya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
